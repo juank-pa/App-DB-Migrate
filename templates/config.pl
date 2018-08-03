8f4b85763afe16ad797832a30c837ce1d2a46568
Juan Carlos Pazmino <juankpro@outlook.com>
2018-08-03 08:08:07 -0400

    Add config

--- templates/config.pl
+++ templates/config.pl
@@ -12,8 +12,16 @@
     # The method receives the table name and the singular form of the table name as paramteres.
     # You can also set this key to a fixed string value.
     # id => sub { "$_[1]_id" },
-
+    #
     # By default migrate generate do not add foreign keys to column references.
     # Uncomment this line to make it add the foreign_key => 1 property.
     # foreign_keys => 1,
+    #
+    # Uncomment the following line if you want to show the options at the end of
+    # the CREATE TABLE statements. Options are added using the options key in create_table.
+    # add_options => 1,
+    #
+    # Some DBAs support a dbspace after the table/index definition.
+    # Uncomment this line if this is the case.
+    # dbspace => 'nsname',
 }
