1cad30f6123d6fda36c1af6ae3265835a090bded
Juan Carlos Pazmino <juankpro@outlook.com>
2018-08-06 01:22:20 -0500

    Implement join_tables

--- lib/Migrate/Handler.pm
+++ lib/Migrate/Handler.pm
@@ -5,8 +5,9 @@ use warnings;
 
 use Migrate::Config;
 use Migrate::Util;
-use Migrate::Factory qw(column timestamp table_index table id reference);
+use Migrate::Factory qw(column timestamp foreign_key table_index table id reference);
 use Migrate::Dbh qw{get_dbh};
+use Lingua::EN::Inflexion qw(noun);
 use DBI;
 
 use feature 'say';
@@ -68,7 +69,7 @@ sub add_timestamps {
 
 sub add_raw_column {
     my ($self, $table, $column) = @_;
-    $self->execute('ALTER TABLE '.id($table, 1).' ADD '.$column);
+    $self->execute('ALTER TABLE '.id($table, 1).' ADD '.id($column));
     $self->_add_indexes($table, $column);
 }
 
@@ -81,7 +82,7 @@ sub drop_table {
 
 sub remove_column {
     my ($self, $table, $column) = @_;
-    $self->execute('ALTER TABLE '.id($table, 1)." DROP $column");
+    $self->execute('ALTER TABLE '.id($table, 1).' DROP '.id($column));
 }
 
 sub remove_columns {
@@ -97,11 +98,16 @@ sub remove_reference {
 
 sub remove_timestamps { $_[0]->remove_columns($_[1], 'created_at', 'updated_at') }
 
+sub _remove_index {
+    my ($self, $index) = @_;
+    $self->execute('DROP INDEX '.id($index->name, 1));
+}
+
 sub remove_index {
-    my ($self, $table, $column, $options) = @_;
-    my $index = table_index($table, $column, $options);
-    my $index_name = $index->name;
-    $self->execute('DROP INDEX '.id($index_name, 1));
+    my ($self, $table, $options_or_column) = @_;
+    my $options = ref($options_or_column) eq 'HASH'? $options_or_column : undef;
+    my $column = $options? 'dummy' : $options_or_column;
+    $self->_remove_index(table_index($table, $column, $options));
 }
 
 sub irreversible {
@@ -114,13 +120,58 @@ sub rename_index;
 sub rename_table;
 sub rename_column;
 
-sub add_foreign_key;
-sub remove_foreign_key;
+sub _add_foreign_key {
+    my ($self, $from_table, $fk) = @_;
+    $self->execute('ALTER TABLE '.id($from_table, 1).' ADD CONSTRAINT FOREIGN KEY ('.$fk->column.') '.$fk);
+}
+
+sub _remove_foreign_key {
+    my ($self, $from_table, $fk) = @_;
+    $self->execute('ALTER TABLE '.id($from_table, 1).' DROP CONSTRAINT '.$fk->name);
+}
+
+sub add_foreign_key {
+    my ($self, $from_table, $to_table, $options) = @_;
+    my $fk = foreign_key($from_table, $to_table, $options);
+    $self->_add_foreign_key($from_table, $fk);
+}
+
+sub remove_foreign_key {
+    my ($self, $from_table, $options_or_to_table) = @_;
+    my $options = ref($options_or_to_table) eq 'HASH'? $options_or_to_table : undef;
+    my $to_table = $options? 'dummy' : $options_or_to_table;
+    my $fk = foreign_key($from_table, $to_table, $options);
+    $self->_remove_foreign_key($from_table, $fk);
+}
 
 sub change_column;
+
 sub change_column_default;
 sub change_column_null;
 
-sub create_join_table;
+sub _join_table_name {
+    my ($self, $table_1, $table_2, $options) = @_;
+    return $options->{table_name} if $options->{table_name};
+    return "${table_1}_$table_2";
+}
+
+sub create_join_table {
+    my ($self, $table_1, $table_2, $options) = @_;
+    $options //= {};
+    $options->{id} = 0;
+    my $col_options = $options->{column_options} // {};
+    $col_options->{index} = 0;
+    my $table_name = $self->_join_table_name($table_1, $table_2, $options);
+    $self->create_table($table_name, $options, sub {
+       my $th = shift;
+       $th->references(noun($table_1)->singular, $col_options);
+       $th->references(noun($table_2)->singular, $col_options);
+    });
+}
+
+sub drop_join_table {
+    my ($self, $table_1, $table_2) = @_;
+    $self->drop_table("${table_1}_$table_2");
+}
 
 return 1;
