dbc20801c20eac432ad7d057603301b4892f1a38
Juan Carlos Pazmino <juankpro@outlook.com>
2018-08-06 01:16:14 -0500

    Add support for Informix temp tables

--- lib/Migrate/Informix/Table.pm
+++ lib/Migrate/Informix/Table.pm
@@ -2,6 +2,7 @@ package Migrate::Informix::Table;
 
 use parent qw(Migrate::Table);
 
+sub temporary { 'TEMP' }
 sub dbspace { Migrate::Config::config->{dbspace}? 'in '.Migrate::Config::config->{dbspace} : undef }
 
 return 1;
